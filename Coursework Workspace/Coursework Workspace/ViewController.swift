012df6872c2f785f2e1c700185d4c734143e52ec
Daniel Garcia Molero <dangar13@icloud.com>
2018-03-16 14:24:57 +0000

    Added animated road background

--- Coursework Workspace/Coursework Workspace/ViewController.swift
+++ Coursework Workspace/Coursework Workspace/ViewController.swift
@@ -10,9 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backgroundImg: UIImageView!
+    
+    var roadArray: [UIImage] = [UIImage(named: "road1.png")!,
+                                UIImage(named: "road2.png")!,
+                                UIImage(named: "road3.png")!,
+                                UIImage(named: "road4.png")!,
+                                UIImage(named: "road5.png")!,
+                                UIImage(named: "road6.png")!,
+                                UIImage(named: "road7.png")!,
+                                UIImage(named: "road8.png")!,
+                                UIImage(named: "road9.png")!,
+                                UIImage(named: "road10.png")!,
+                                UIImage(named: "road11.png")!,
+                                UIImage(named: "road12.png")!,
+                                UIImage(named: "road13.png")!,
+                                UIImage(named: "road14.png")!,
+                                UIImage(named: "road15.png")!,
+                                UIImage(named: "road16.png")!,
+                                UIImage(named: "road17.png")!,
+                                UIImage(named: "road18.png")!,
+                                UIImage(named: "road19.png")!,
+                                UIImage(named: "road20.png")!]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setAnimatedBackground()
+        displayMenu() //To display the game menu.
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +44,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func setAnimatedBackground() {
+        backgroundImg.image = UIImage.animatedImage(with: roadArray, duration: 0.5)
+    }
+    
+    func displayMenu() {
+        //On click, call hideMenu
+    }
+    
+    func hideMenu() {
+        //view.isHidden = true; to make an app seem like it has more than one view
+    }
 }
-
