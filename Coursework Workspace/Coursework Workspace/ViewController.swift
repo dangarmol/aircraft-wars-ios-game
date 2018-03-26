c2b983f9b7db3a3d734edbb8f3dab986f0682756
Daniel Garcia Molero <dangar13@icloud.com>
2018-03-26 23:07:04 +0100

    Major changes to sources folder and added all necessary images.
    Edited all images on Photoshop and added them.

--- Coursework Workspace/Coursework Workspace/ViewController.swift
+++ Coursework Workspace/Coursework Workspace/ViewController.swift
@@ -14,32 +14,32 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var planeImg: UIImageView!
     
-    var backgroundArray: [UIImage] = [UIImage(named: "road1.png")!,
-                                UIImage(named: "road2.png")!,
-                                UIImage(named: "road3.png")!,
-                                UIImage(named: "road4.png")!,
-                                UIImage(named: "road5.png")!,
-                                UIImage(named: "road6.png")!,
-                                UIImage(named: "road7.png")!,
-                                UIImage(named: "road8.png")!,
-                                UIImage(named: "road9.png")!,
-                                UIImage(named: "road10.png")!,
-                                UIImage(named: "road11.png")!,
-                                UIImage(named: "road12.png")!,
-                                UIImage(named: "road13.png")!,
-                                UIImage(named: "road14.png")!,
-                                UIImage(named: "road15.png")!,
-                                UIImage(named: "road16.png")!,
-                                UIImage(named: "road17.png")!,
-                                UIImage(named: "road18.png")!,
-                                UIImage(named: "road19.png")!,
-                                UIImage(named: "road20.png")!]
+    var backgroundArray: [UIImage] = [  UIImage(named: "road1.png")!,
+                                        UIImage(named: "road2.png")!,
+                                        UIImage(named: "road3.png")!,
+                                        UIImage(named: "road4.png")!,
+                                        UIImage(named: "road5.png")!,
+                                        UIImage(named: "road6.png")!,
+                                        UIImage(named: "road7.png")!,
+                                        UIImage(named: "road8.png")!,
+                                        UIImage(named: "road9.png")!,
+                                        UIImage(named: "road10.png")!,
+                                        UIImage(named: "road11.png")!,
+                                        UIImage(named: "road12.png")!,
+                                        UIImage(named: "road13.png")!,
+                                        UIImage(named: "road14.png")!,
+                                        UIImage(named: "road15.png")!,
+                                        UIImage(named: "road16.png")!,
+                                        UIImage(named: "road17.png")!,
+                                        UIImage(named: "road18.png")!,
+                                        UIImage(named: "road19.png")!,
+                                        UIImage(named: "road20.png")!]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         displayMenu() //To display the game menu.
         loadGraphics()
-        startGame()
+        startGameEngine()
         displayMenu() //To display the game menu.
     }
 
@@ -53,7 +53,7 @@ class ViewController: UIViewController {
         //planeImg.image.
     }
     
-    func startGame() {
+    func startGameEngine() {
         
     }
     
